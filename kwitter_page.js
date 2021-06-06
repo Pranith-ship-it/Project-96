25d84676e9209cb971b1fa983086e8d7d3a39e90
Pranith-ship-it <79912809+Pranith-ship-it@users.noreply.github.com>
2021-06-06 09:15:54 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,15 +10,16 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+
+  user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
 function send(){
-    msg=document.getElementById("msg").value;
-    firebase.database().ref(room_name).push({
+  msg=document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
     name:user_name,
     message:msg,
     like:0
-    });
-
-    document.getElementById("msg").value="";
+  });
 }
-
- user_name = localStorage.getItem("user_name");
